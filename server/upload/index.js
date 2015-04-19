f25c11c5c15d684b2b188271acb2956d430c069f
mithipati <neil.mithipati@gmail.com>
2015-04-19 13:52:47 -0700

    Upload local file

--- server/upload/index.js
+++ server/upload/index.js
@@ -1,20 +1,36 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
+/* Declate dependencies */
+var express = require('express'),
+	router = express.Router(),
+	fs = require('fs'),
+	s3 = require('s3');
 
-var s3 = require('s3');
-
+/* Create S3 client */
 var client = s3.createClient({
 	s3Options: {
-		accessKeyId: process.env.AWS_ACCESS_KEY,
-    	secretAccessKey: process.env.AWS_SECRET_KEY
+		accessKeyId: 'AKIAJXYEMJT7W4ZVXWRQ',
+    	secretAccessKey: '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'
 	}
 });
 
+/* Handle user request */
 router.post('/', function (req, res) {
+	// console.log(__dirname + '/../../client/assets/images/pdf.icon.png');
 	console.log(req);
+	/* Init upload of client file */
+	var uploader = client.uploadFile({
+		localFile: __dirname + '/../../client/assets/images/pdf.icon.png',
+		s3Params: {
+			Bucket: 'esnail',
+			Key: 'Sample1'
+		}
+	});
+	/* Log when upload complete */
+	uploader.on('end', function() {
+		console.log('done uploading');
+	});
+	/* End request-response cycle */
 	res.end();
 });
 
